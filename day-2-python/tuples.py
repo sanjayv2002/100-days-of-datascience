9cafe0ec28a2252c1f9b4ad0a1fffbb4ae83f1cb
sanjayv2002 <sanjay.murthy.29@gmail.com>
2023-03-27 10:58:59 +0530

    day-2-python

---
+++ day-2-python/tuples.py
@@ -0,0 +1,60 @@
+empty_tuple = ()
+
+# OR
+
+empty_tuple = tuple()
+
+
+# Initializing values for tuple
+fruits = ('banana', 'orange', 'mango', 'lemon')
+
+# printing the length of the tuple
+print(len(fruits))
+
+# positive and negative indexing of tuples
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[last_index]
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+last_fruit = fruits[-1]
+
+# Slicing the tuples with positive and negative indexing
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[0:4]    # all items
+all_fruits= fruits[0:]      # all items
+orange_mango = fruits[1:3]  # doesn't include item at index 3
+orange_to_the_rest = fruits[1:]
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[-4:]    # all items
+orange_mango = fruits[-3:-1]  # doesn't include item at index 3
+orange_to_the_rest = fruits[-3:]
+
+# tuples to list
+fruits = ('banana', 'orange', 'mango', 'lemon')
+fruits = list(fruits)
+fruits[0] = 'apple'
+print(fruits)     # ['apple', 'orange', 'mango', 'lemon']
+fruits = tuple(fruits)
+print(fruits)     # ('apple', 'orange', 'mango', 'lemon')
+
+# Accessing and assigning an item in tuple
+fruits = ('banana', 'orange', 'mango', 'lemon')
+print('orange' in fruits) # True
+print('apple' in fruits) # False
+# fruits[0] = 'apple' # TypeError: 'tuple' object does not support item assignment
+
+# Concatenating tuple
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+vegetables = ('Tomato', 'Potato', 'Cabbage','Onion', 'Carrot')
+fruits_and_vegetables = fruits + vegetables
+
+# Deleting tuples
+fruits = ('banana', 'orange', 'mango', 'lemon')
+del fruits
